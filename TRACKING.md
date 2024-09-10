b5cd0459e13d5dd82a3f44b9ad44e09a28c4bb75
apuzyk <apuzyk@gmail.com>
2024-09-09 20:34:58 -0700

    finishing up the writing of lines to the LED displays

--- TRACKING.md
+++ TRACKING.md
@@ -1,7 +1,9 @@
-2024-09-02
+## 2024-09-02
 * Implemented the RGB matrix library bindings
 * TODO
   * add write test in main
   * Compile it on the raspberry pi
   * Run the write test on the raspberry pi
   * Add write method for the busses
+* Thoughts
+  * Should we use rpi-rgb-led-matrix/fonts/4x6.bdf size font this will let us do 4 rows comfortably.  the 5x8 could also work
--- src/display_board.rs
+++ src/display_board.rs
@@ -22,6 +22,74 @@ pub struct DisplayBoard {
     pub font: Option<Font>,
 }
 
+pub struct RGBDisplayLine {
+    line: Vec<StyledString>,
+}
+
+impl RGBDisplayLine {
+    pub fn new() -> Self {
+        RBDDisplayLine {
+            line: Vec<StyledString>::new(),
+        }
+    }
+}
+
+pub struct StyledString {
+    string: String,
+    color: Color,
+}
+
+impl StyledString {
+    pub fn new() -> Self {
+        StyledString {
+            string: "".to_string(),
+            color: LedColor::new(),
+        }
+    }
+}
+
+pub struct LedColor {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+impl LedColor {
+    pub fn new() -> Self {
+        LedColor {
+            red: 0, 
+            green: 0, 
+            blue: 0,
+        }
+    }
+
+    pub fn red() -> Self {
+        LedColor {
+            red: 255,
+            green: 0,
+            blue: 0,
+        }
+    }
+
+    pub fn green() -> Self {
+        LedColor{
+            red: 0,
+            green: 255,
+            blue: 0,
+        }
+    }
+
+    pub fn blue() -> Self {
+        LedColor {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+}
+
+const COL_WIDTH: i32 = 64;
+
 impl DisplayBoard {
     pub fn new() -> Self {
         DisplayBoard {
@@ -60,7 +128,144 @@ impl DisplayBoard {
     }
     pub fn test_write(self) {
         let mut canvas = self.rgb_matrix.get_canvas();
-        canvas.draw_text(&self.font, 0, 10, 0, 0, 255, "Hello, World!", 0);
+        canvas.draw_text(&self.font, 0, 6, 0, 0, 255, "Hello, World!", 0);
+    }
+    pub fn write_lines(self) {
+        self.write_times();
+        self.write_bus_times();
+    }
+    pub fn write_times(self) {
+        let mut canvas = self.rgb_matrix.get_canvas();
+
+        let font_height = self.font.height();
+        curr_row = font_height;
+        curr_time = String::from("Current Time: ");
+        let now = Local::now();
+        if (now.month() == 2 && now.day() == 2) {
+            curr_time.push_str(String::from("YOUR BIRTHDAY!"));
+        } else {
+            curr_time.push_str(now.format("%H:%M:%S").to_string());
+        }
+        canvas.draw_text(&self.font, 0, curr_row, 0, 0, 255, curr_time, 0);
+
+        if let Some(request_time) = self.last_successful_request_time {
+            last_updated = String::from("Last Updated: ");
+            last_updated.push_str(request_time.format("%H:%M:%S").to_string());
+            canvas.draw_text(&self.font, COL_WIDTH, curr_row, 0, 0, 255, last_updated, 0);
+        }
+
+
+        curr_row += font_height + 1;
+
+        let lines_to_write = self.get_bus_styled_lines();
+        
+        for (index, line) in lines_to_write.iter().enumerate() {
+            for styled_str in line.line {
+                let col_pos = if index % 2 == 1 {0} else {COL_WIDTH};
+                canvas.draw_text(&self.font, col_pos, cur_row, styled_str.color.red, styled_str.color.green, styled_str.color.blue, styled_str.string, 0);
+                if index %2 == 0 {
+                    curr_row += font_height + 1;
+                }
+            } 
+        }
+    }
+
+    pub fn lines_to_write(self) -> Vec<String> {
+        
+    }
+    pub fn get_bus_styled_lines(self) -> Vec<RGBDisplayLine> {
+        let lines = Vec<RGBDisplayLine>::new();
+        let display_lines = match &self.display_lines {
+            Some(dl) => dl,
+            None => return output,
+        };
+
+        let mut sorted_keys = Vec::new();
+        for key in display_lines.keys() {
+            sorted_keys.push(key.clone());
+        }
+        sorted_keys.sort();
+
+        let longest_key = match sorted_keys.iter().map(|key| key.chars().count()).max() {
+            Some(val) => val + 1,
+            None => 0,
+        };
+
+        for key in sorted_keys {
+            let mut this_line = RGBDisplayLine::new();
+            this_line.line.push(
+                StyledString {
+                    string: key.clone(),
+                    color: LedColor.blue(),
+                },
+            );
+
+            this_line.line.push(
+                StyledString{
+                    string: ":".to_string(),
+                    color: LedColor.blue(),
+                }
+            );
+            
+            for _ in 0..(longest_key - key.chars().count()) {
+                this_line.line.push(
+                    StyledString{
+                        string: " ".to_string(),
+                        color: LedColor.blue(),
+                    }
+                );
+            }
+            for mvj in &display_lines[&key] {
+                match mvj.time_to_arrival() {
+                    Some(tta) => {
+                        // rapid lines the line ref isn't the same
+                        // as the key
+                        if mvj.line_ref != key {
+                            if mvj.has_location() {
+                                this_line.line.push(
+                                    StyledString {
+                                        string: tta.to_string(),
+                                        color: LedColor.red(),
+                                    }
+                                );
+                            } else {
+                                this_line.line.push(
+                                    StyledString {
+                                        string: tta.to_string(),
+                                        color: LedColor.red()
+                                    }
+                                );
+                            }
+                        } else {
+                            if mvj.has_location() {
+                                this_line.line.push(
+                                    StyledString {
+                                        string: tta.to_string(),
+                                        color: LedColor.blue(),
+                                    }
+                                );
+                            } else {
+                                this_line.line.push(
+                                    StyledString {
+                                        string: tta.to_string(),
+                                        color: LedColor.blue(),
+                                    }
+                                );
+                            }
+                        }
+                    }
+                    None => (),
+                }
+                this_line.line.push(
+                    StyleString {
+                        string: " ".to_string(),
+                        color: LedColor.blue(),
+                    }
+                )
+            }
+            lines.push(this_line);
+        }
+        return lines
     }
 }
 
